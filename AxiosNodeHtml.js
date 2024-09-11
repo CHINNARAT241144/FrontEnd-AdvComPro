5ec0360ca2d3c1049c13be5fbc2477cffdfc9034
CHINNARAT241144 <dew.eyecandy@gmail.com>
2024-09-11 09:22:48 +0700

    test.

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const axios = require('axios');
+const express = require ('express');
+const axios = require ('axios');
 const app = express();
-var bodyParser = require('body-parser'); 
+var bodyParser = require('body-parser');
+const path = require("path")
 
-const base_url = "http://localhost:3000"
+const base_url = "http://localhost:3000";
 
+app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(__dirname + '/public'));
 
-
 app.get("/", async (req, res) => {
     try {
-        const response = await axios.get(base_url + '/books'); // แก้ไข URL
-        res.render("books", { books: response.data }); // สมมติว่าเราจะเรนเดอร์รายชื่อหนังสือที่หน้าแรก
+        const response = await axios.get(base_url + '/books');
+        res.render("books", { books: response.data });
     } catch (err) {
         console.error(err);
-        res.status(500).send("Error");
+        res.status(500).send('Error');
     }
 });
 
-app.get("/books/:id", async (req, res) => {
+app.get("/book/:id", async (req, res) => {
     try {
         const response = await axios.get(base_url + '/books/' + req.params.id);
-        res.render("books", { book: response.data }); // ใช้ชื่อให้สอดคล้อง
+        res.render("book", { book: response.data });
     } catch (err) {
         console.error(err);
-        res.status(500).send("Error");
+        res.status(500).send('Error');
     }
 });
 
@@ -43,41 +44,42 @@ app.post("/create", async (req, res) => {
         res.redirect("/");
     } catch (err) {
         console.error(err);
-        res.status(500).send("Error");
+        res.status(500).send('Error');
     }
 });
 
 app.get("/update/:id", async (req, res) => {
     try {
-        const response = await axios.get(base_url + '/books/' + req.params.id);
-        res.render("update", { book: response.data }); // ใช้ชื่อให้สอดคล้อง
+        const response = await axios.get(
+        base_url + '/books/' + req.params.id);
+        res.render("update", { book: response.data });
     } catch (err) {
         console.error(err);
-        res.status(500).send("Error");
+        res.status(500).send('Error');
     }
 });
 
 app.post("/update/:id", async (req, res) => {
     try {
         const data = { title: req.body.title, author: req.body.author };
-        await axios.put(base_url + '/books/' + req.params.id, data); // แก้ไข URL
+        await axios.put(base_url + '/books/' + req.params.id, data);
         res.redirect("/");
     } catch (err) {
         console.error(err);
-        res.status(500).send("Error");
+        res.status(500).send('Error');
     }
 });
 
 app.get("/delete/:id", async (req, res) => {
     try {
         await axios.delete(base_url + '/books/' + req.params.id);
-        res.redirect("/");
+            res.redirect("/");
     } catch (err) {
         console.error(err);
-        res.status(500).send("Error");
+        res.status(500).send('Error');
     }
 });
 
 app.listen(5500, () => {
     console.log('Server started on port 5500');
-});
+})
